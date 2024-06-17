64571bbd33cc24381120445f1476de4eba4c7983
Oumezzine Farah <oumezzine.farah@yahoo.com>
2024-06-17 10:48:56 +0100

    delete firstComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstcomponentComponent } from './firstcomponent/firstcomponent.component';
 import { FormsModule } from '@angular/forms';
 import { AjoutComponent } from './ajout/ajout.component';
 import { ListComponent } from './list/list.component';
@@ -11,7 +10,6 @@ import { ListComponent } from './list/list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FirstcomponentComponent,
     AjoutComponent,
     ListComponent
   ],
